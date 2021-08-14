909a4414938dd924008ce11aff7c2aa0b49ebe63
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-08-14 11:44:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/subinps/MusicPlayer.git /MusicPlayer
+git clone https://github.com/HellBoy-OP/OwO.git /MusicPlayer
 cd /MusicPlayer
 echo "Starting Bot...."
 python3 main.py
