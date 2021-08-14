b5455856be0c9cad7a14389c3cb96baf2fa8bc30
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-08-14 12:07:26 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -1,25 +1,3 @@
-#MIT License
-
-#Copyright (c) 2021 SUBIN
-
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
-
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
-
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram import Client, emoji
 from utils import mp
@@ -28,38 +6,34 @@ playlist=Config.playlist
 
 HELP = """
 
-<b>Add the bot and User account in your Group with admin rights.
+<b>✓ Add bot and helper account in your Group with admin rights.
 
-Start a VoiceChat
+✓ Start a VoiceChat.</b>
 
-Use /play <song name> or use /play as a reply to an audio file or youtube link.
+**📍 Common Commands :**
 
-You can also use /dplay <song name> to play a song from Deezer.</b>
+• `/play` - __Reply to an audio file or YouTube link to play it or use /play <song name>.__
+• `/dplay` - __Play music from Deezer, Use /dplay <song name>__
+• `/player` - __Show current playing song.__
+• `/help` - __Show help for commands__
+• `/playlist` - __Shows the playlist.__
 
-**Common Commands**:
-
-**/play**  Reply to an audio file or YouTube link to play it or use /play <song name>.
-**/dplay** Play music from Deezer, Use /dplay <song name>
-**/player**  Show current playing song.
-**/help** Show help for commands
-**/playlist** Shows the playlist.
-
-**Admin Commands**:
-**/skip** [n] ...  Skip current or n where n >= 2
-**/join**  Join voice chat.
-**/leave**  Leave current voice chat
-**/vc**  Check which VC is joined.
-**/stop**  Stop playing.
-**/radio** Start Radio.
-**/stopradio** Stops Radio Stream.
-**/replay**  Play from the beginning.
-**/clean** Remove unused RAW PCM files.
-**/pause** Pause playing.
-**/resume** Resume playing.
-**/volume** Change volume(0-200).
-**/mute**  Mute in VC.
-**/unmute**  Unmute in VC.
-**/restart**Update restarts the Bot.
+**📍 Admin Commands :**
+• `/skip` - __Skip current or n where n >= 2__
+• `/join` - __Join voice chat.__
+• `/leave` - __Leave current voice chat.__
+• `/vc` - __Check which VC is joined.__
+• `/stop` - __Stop playing.__
+• `/radio` - __Start Radio.__
+• `/stopradio` - __Stops Radio Stream.__
+• `/replay` - __Play from the beginning.__
+• `/clean` - __Remove unused RAW PCM files.__
+• `/pause` - __Pause playing.__
+• `/resume` - __Resume playing.__
+• `/volume` - __Change volume(0-200).__
+• `/mute` - __Mute in VC.__
+• `/unmute` - __Unmute in VC.__
+• `/restart` - __Update restarts the Bot.__
 """
 
 
@@ -69,7 +43,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     admins = await mp.get_admins(Config.CHAT)
     if query.from_user.id not in admins and query.data != "help":
         await query.answer(
-            "😒 Played Joji.mp3",
+            "😒",
             show_alert=True
             )
         return
@@ -83,8 +57,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         if not playlist:
             pl = f"{emoji.NO_ENTRY} Empty Playlist"
         else:
-            pl = f"{emoji.PLAY_BUTTON} **Playlist**:\n" + "\n".join([
-                f"**{i}**. **🎸{x[1]}**\n   👤**Requested by:** {x[4]}"
+            pl = f"{emoji.PLAY_BUTTON} **Playlist :**\n" + "\n".join([
+                f"**{i}**. **🎸 {x[1]}**\n   👤 **Requested by :** {x[4]}"
                 for i, x in enumerate(playlist)
                 ])
         await query.edit_message_text(
@@ -107,8 +81,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             return
         else:
             mp.group_call.pause_playout()
-            pl = f"{emoji.PLAY_BUTTON} **Playlist**:\n" + "\n".join([
-                f"**{i}**. **🎸{x[1]}**\n   👤**Requested by:** {x[4]}"
+            pl = f"{emoji.PLAY_BUTTON} **Playlist :**\n" + "\n".join([
+                f"**{i}**. **🎸 {x[1]}**\n   👤 **Requested by :** {x[4]}"
                 for i, x in enumerate(playlist)
                 ])
         await query.edit_message_text(f"{emoji.PLAY_OR_PAUSE_BUTTON} Paused\n\n{pl}",
@@ -130,8 +104,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             return
         else:
             mp.group_call.resume_playout()
-            pl = f"{emoji.PLAY_BUTTON} **Playlist**:\n" + "\n".join([
-                f"**{i}**. **🎸{x[1]}**\n   👤**Requested by:** {x[4]}"
+            pl = f"{emoji.PLAY_BUTTON} **Playlist :**\n" + "\n".join([
+                f"**{i}**. **🎸 {x[1]}**\n   👤 **Requested by :** {x[4]}"
                 for i, x in enumerate(playlist)
                 ])
         await query.edit_message_text(f"{emoji.PLAY_OR_PAUSE_BUTTON} Resumed\n\n{pl}",
@@ -152,8 +126,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             return
         else:
             await mp.skip_current_playing()
-            pl = f"{emoji.PLAY_BUTTON} **Playlist**:\n" + "\n".join([
-                f"**{i}**. **🎸{x[1]}**\n   👤**Requested by:** {x[4]}"
+            pl = f"{emoji.PLAY_BUTTON} **Playlist :**\n" + "\n".join([
+                f"**{i}**. **🎸 {x[1]}**\n   👤 **Requested by :** {x[4]}"
                 for i, x in enumerate(playlist)
                 ])
         try:
@@ -174,12 +148,12 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data=="help":
         buttons = [
             [
-                InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-                InlineKeyboardButton('🤖 Other Bots', url='https://t.me/subin_works/122'),
+                InlineKeyboardButton('Channel', url='https://t.me/its_hellbot'),
+                InlineKeyboardButton('Github', url='https://github.com/The-HellBot'),
             ],
             [
-                InlineKeyboardButton('👨🏼‍💻 Developer', url='https://t.me/subinps'),
-                InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/MusicPlayer'),
+                InlineKeyboardButton('Owner', url='https://t.me/ForGo10God'),
+                InlineKeyboardButton('Source', url='https://t.me/its_hellbot'),
             ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
